b1aef2cb3426c8a894a2de433c86571d5354f4d1
holgus103 <holgus103@gmail.com>
2017-06-04 17:39:49 +0200

    Subboard generation and evaluation

--- Board.R
+++ Board.R
@@ -1,33 +1,44 @@
 boardsize <- 3
 winningSeries <- 3
 
-iInitBoard <- function(){
+initBoard <- function(){
   return(matrix(0, ncol = boardsize, nrow = boardsize))
 }
 
 checkForVictory <- function(board){
+  checkSubMatrix <- function(board){
+    secondDiagonal <- function(m){
+      return(diag(apply(m, 2, rev)))
+    }
+    maxSequence = max(
+      # check first row
+      sum(board[1,]),
+      # check first column
+      sum(board[,1]),
+      # check diagonal
+      sum(diag(board)),
+      # check other diagonal
+      sum(secondDiagonal(board))
+    )
+    if(abs(maxSequence) == winningSeries){
+      return(maxSequence/winningSeries)
+    }
+    else{
+      return(0)
+    }
+  }
   
+  getSubMatrix <- function(board, uppery, leftx){
+    return(board[c(leftx:(leftx+winningSeries-1)), c(uppery:(uppery+winningSeries-1))])
+  }
+  
+  corners = expand.grid(c(1:(boardsize - winningSeries + 1)), c(1:(boardsize - winningSeries + 1)))
+  res = apply(corners, 1, function(i){
+    checkSubMatrix(getSubMatrix(board, i[1], i[1]))
+  })
+  
+  # TODO: check the remaining right columns and the bottom rows
+  
+      
 }
 
-checkSubMatrix <- function(board){
-  secondDiagonal <- function(m){
-    return(diag(apply(m, 2, rev)))
-  }
-   maxSequence = max(
-              # check first row
-              sum(board[1,]),
-              # check first column
-              sum(board[,1]),
-              # check diagonal
-              sum(diag(board)),
-              # check other diagonal
-              sum(secondDiagonal(board))
-             )
-   if(abs(maxSequence) == winningSeries){
-     return(maxSequence/winningSeries)
-   }
-   else{
-     return(0)
-   }
-         
-}
